f94905fee99c6cf976ce0840d4e16c4021e3f3b8
Erik421 <erikmirzoyan22@gmail.com>
2025-02-02 21:16:23 +0300

    first commit

--- db.py
+++ db.py
@@ -1,39 +1,60 @@
 import os
-import sqlite3
+import psycopg2
 
-DB_NAME = 'inst_bot.db'
+
+DB_NAME = 'inst_bot'
+DB_USER = 'Erik'
+DB_PASSWORD = '12456'
+DB_HOST = '127.0.0.1'
+DB_PORT = '5432'
+
 if not os.path.exists('photos'):
     os.makedirs('photos')
 
 
 def init_db():
-    conn = sqlite3.connect(DB_NAME)
+    conn = psycopg2.connect(
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        host=DB_HOST,
+        port=DB_PORT,
+        # options="-c client_encoding=UTF8"
+    )
     cursor = conn.cursor()
+
+
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS users (
-            user_id INTEGER PRIMARY KEY AUTOINCREMENT,
-            tg_id INTEGER NOT NULL,
-            username TEXT UNIQUE
+            user_id SERIAL PRIMARY KEY,
+            tg_id BIGINT NOT NULL,
+            username VARCHAR(255) UNIQUE
         )
     ''')
     conn.commit()
+
+
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS photos (
-            photo_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            photo_id SERIAL PRIMARY KEY,
             user_id INTEGER,
             photo TEXT NOT NULL,
             number INTEGER,
-            FOREIGN KEY (user_id) REFERENCES users (user_id)
+            FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE
         )
     ''')
     conn.commit()
+
+
     cursor.execute('''
         CREATE TABLE IF NOT EXISTS subscriptions (
-            subscription_id INTEGER PRIMARY KEY AUTOINCREMENT,
-            sub_username TEXT UNIQUE,
+            subscription_id SERIAL PRIMARY KEY,
+            sub_username VARCHAR(255) UNIQUE,
             user_id INTEGER,
-            FOREIGN KEY (user_id) REFERENCES users (user_id)
+            FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE
         )
     ''')
+    conn.commit()
 
-
+    cursor.close()
+    conn.close()
--- requests.py
+++ requests.py
@@ -1,88 +1,107 @@
 from db import *
 
 
+def connect_db():
+    return psycopg2.connect(
+        dbname=DB_NAME,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        host=DB_HOST,
+        port=DB_PORT
+    )
+
+
 def add_user(username, tg_id):
-    conn = sqlite3.connect(DB_NAME)
+    conn = connect_db()
     cursor = conn.cursor()
-    cursor.execute('INSERT OR IGNORE INTO users (username, tg_id) VALUES (?, ?)', (username, tg_id))
+    cursor.execute('INSERT INTO users (username, tg_id) VALUES (%s, %s) ON CONFLICT (username) DO NOTHING', (username, tg_id))
     conn.commit()
+    cursor.close()
     conn.close()
 
 
 def add_photo(user_id, photo):
-    conn = sqlite3.connect(DB_NAME)
+    conn = connect_db()
     cursor = conn.cursor()
-    cursor.execute('INSERT INTO photos (user_id, photo) VALUES (?, ?)', (user_id, photo))
+    cursor.execute('INSERT INTO photos (user_id, photo) VALUES (%s, %s)', (user_id, photo))
     conn.commit()
+    cursor.close()
     conn.close()
 
 
 def get_user_photos(user_id):
-    conn = sqlite3.connect(DB_NAME)
+    conn = connect_db()
     cursor = conn.cursor()
-    cursor.execute('SELECT photo FROM photos WHERE user_id = ?', (user_id,))
+    cursor.execute('SELECT photo FROM photos WHERE user_id = %s', (user_id,))
     photos = cursor.fetchall()
+    cursor.close()
     conn.close()
     return [photo[0] for photo in photos]
 
 
 def update_number(user_id):
-    conn = sqlite3.connect(DB_NAME)
+    conn = connect_db()
     cursor = conn.cursor()
-    cursor.execute('SELECT photo_id FROM photos WHERE user_id = ? ORDER BY number', (user_id,))
+    cursor.execute('SELECT photo_id FROM photos WHERE user_id = %s ORDER BY number', (user_id,))
     photos = cursor.fetchall()
     for new_number, photo in enumerate(photos, start=1):
         photo_id = photo[0]
-        cursor.execute('UPDATE photos SET number = ? WHERE photo_id = ?', (new_number, photo_id))
+        cursor.execute('UPDATE photos SET number = %s WHERE photo_id = %s', (new_number, photo_id))
     conn.commit()
+    cursor.close()
     conn.close()
 
 
 def remove_photo(user_id, number):
-    conn = sqlite3.connect(DB_NAME)
+    conn = connect_db()
     cursor = conn.cursor()
     update_number(user_id)
-    cursor.execute('SELECT photo FROM photos WHERE user_id = ? AND number = ?', (user_id, number,))
+    cursor.execute('SELECT photo FROM photos WHERE user_id = %s AND number = %s', (user_id, number,))
     result = cursor.fetchone()
     photo_path = result[0]
-    cursor.execute('DELETE FROM photos WHERE user_id = ? AND number = ?', (user_id, number))
+    cursor.execute('DELETE FROM photos WHERE user_id = %s AND number = %s', (user_id, number))
     conn.commit()
+    cursor.close()
     conn.close()
     os.remove(photo_path)
 
 
 def add_subscription(sub_username, user_id):
-    conn = sqlite3.connect(DB_NAME)
+    conn = connect_db()
     cursor = conn.cursor()
-    cursor.execute('INSERT INTO subscriptions (sub_username, user_id) VALUES (?, ?)'
-                   'ON CONFLICT(sub_username) DO UPDATE SET user_id = excluded.user_id', (sub_username, user_id))
+    cursor.execute('''INSERT INTO subscriptions (sub_username, user_id) VALUES (%s, %s)
+                   ON CONFLICT(sub_username) DO UPDATE SET user_id = excluded.user_id''', (sub_username, user_id))
     conn.commit()
+    cursor.close()
     conn.close()
 
 
 def get_user_subscriptions(user_id):
-    conn = sqlite3.connect(DB_NAME)
+    conn = connect_db()
     cursor = conn.cursor()
-    cursor.execute('SELECT sub_username FROM subscriptions WHERE user_id = ?', (user_id,))
+    cursor.execute('SELECT sub_username FROM subscriptions WHERE user_id = %s', (user_id,))
     subscriptions = cursor.fetchall()
+    cursor.close()
     conn.close()
     return [subscription[0] for subscription in subscriptions]
 
 
 def get_user_id(username):
-    conn = sqlite3.connect(DB_NAME)
+    conn = connect_db()
     cursor = conn.cursor()
-    cursor.execute('SELECT user_id FROM users WHERE username = ?', (username,))
+    cursor.execute('SELECT user_id FROM users WHERE username = %s', (username,))
     user_id = cursor.fetchone()
+    cursor.close()
     conn.close()
     return user_id[0] if user_id else None
 
 
 def is_valid_username(username):
-    conn = sqlite3.connect(DB_NAME)
+    conn = connect_db()
     cursor = conn.cursor()
-    cursor.execute('SELECT COUNT(*) FROM users WHERE username = ?', (username,))
+    cursor.execute('SELECT COUNT(*) FROM users WHERE username = %s', (username,))
     count = cursor.fetchone()[0]
+    cursor.close()
     conn.close()
     return bool(count)
 
@@ -93,17 +112,19 @@ def subscribe_user(user_id, username):
 
 
 def unsubscribe_user(user_id, sub_username):
-    conn = sqlite3.connect(DB_NAME)
+    conn = connect_db()
     cursor = conn.cursor()
-    cursor.execute('DELETE FROM subscriptions WHERE user_id = ? AND sub_username = ?', (user_id, sub_username))
+    cursor.execute('DELETE FROM subscriptions WHERE user_id = %s AND sub_username = %s', (user_id, sub_username))
     conn.commit()
+    cursor.close()
     conn.close()
 
 
 def get_all_users():
-    conn = sqlite3.connect(DB_NAME)
+    conn = connect_db()
     cursor = conn.cursor()
     cursor.execute('SELECT tg_id FROM users')
     users = cursor.fetchall()
+    cursor.close()
     conn.close()
     return [{'tg_id': user[0]} for user in users]
